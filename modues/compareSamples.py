cd93947fdb89a5221c0dd956ce9faf84a9a22ed7
maho10 <69826606+maho10@users.noreply.github.com>
2021-05-19 23:52:47 -0600

    Add files via upload

---
+++ modues/compareSamples.py
@@ -0,0 +1,37 @@
+# Proyecto de Herramientas
+# Comparar muestras de ADN
+
+
+
+
+
+def compareDna(dna, sample):
+    dnaTP0X1 = dna[0]
+    dnaTP0X2 = dna[1]
+    dnaFGA1 = dna[2]
+    dnaFGA2 = dna[3]
+    dnaTH011 = dna[4]
+    dnaTH012 = dna[5]
+    dnaVWA1 = dna[6]
+    dnaVWA2 = dna[7]
+    dnaD13S3171 = dna[8]
+    dnaD13S3172 = dna[9]
+    dnaD21S111 = dna[10]
+    dnaD21S112 = dna[11]
+    dnaAMEL1 = dna[12]
+    dnaAMEL2 = dna[13]
+
+    sampleTP0X1 = sample[0]
+    sampleTP0X2 = sample[1]
+    sampleFGA1 = sample[2]
+    sampleFGA2 = sample[3]
+    sampleTH011 = sample[4]
+    sampleTH012 = sample[5]
+    sampleVWA1 = sample[6]
+    sampleVWA2 = sample[7]
+    sampleD13S3171 = sample[8]
+    sampleD13S3172 = sample[9]
+    sampleD21S111 = sample[10]
+    sampleD21S112 = sample[11]
+    sampleAMEL1 = sample[12]
+    sampleAMEL2 = sample[13]
